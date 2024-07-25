3158ffc199e7e06d08edb92e08e30bd7319808da
anaresgalla <anaresgalla@gmail.com>
2024-07-25 10:46:28 -0300

    Adds humidity image and text

--- src/WeatherAppGui.java
+++ src/WeatherAppGui.java
@@ -68,6 +68,17 @@ public class WeatherAppGui extends JFrame {
         weatherConditionDesc.setFont(new Font("Dialog", Font.PLAIN, 32));
         weatherConditionDesc.setHorizontalAlignment(SwingConstants.CENTER);
         add(weatherConditionDesc);
+
+        //humidity image
+        JLabel humidityImage = new JLabel(loadImage("src/assets/humidity.png"));
+        humidityImage.setBounds(15, 500, 74, 66);
+        add(humidityImage);
+
+        // humidity text
+        JLabel humidityText = new JLabel("<html><b>Humidity:</b> 100%</html>");
+        humidityText.setBounds(90, 500, 85, 55);
+        humidityText.setFont(new Font("Dialog",Font.PLAIN, 16));
+        add(humidityText);
     }
 
     //used to create images in our gui component:
